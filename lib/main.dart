8c51016c95c51786fe4e706dce38d0ffad5264e1
Theo <119578352+BoulinguezTheo@users.noreply.github.com>
2023-05-11 16:42:28 +0200

    séparer la bottomnavbar dans un fichier

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:sg_mobile/services/service_locator.dart';
+import 'package:sg_mobile/widgets/bottom_navigation_bar.dart';
 import 'package:sg_mobile/widgets/game_list_view.dart';
 import 'package:sg_mobile/widgets/login.dart';
 import 'package:sg_mobile/widgets/square_game_app_bar.dart';
@@ -19,12 +20,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int selectedIndex = 0;
-  void onItemTapped(int index) {
-    setState(() {
-      selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,25 +39,7 @@ class _MyAppState extends State<MyApp> {
         ),
         '/home': (context) => Scaffold(
           body: GameListWidget(),
-          bottomNavigationBar: BottomNavigationBar(
-            selectedItemColor: Colors.red,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.view_agenda),
-                label: 'Parties en cours',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.videogame_asset),
-                label: 'jeux',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                label: 'Mon compte',
-              ),
-            ],
-            currentIndex: selectedIndex,
-            onTap: onItemTapped,
-          ),
+          bottomNavigationBar: CustomBottomNavigationBar(),
         ),
       },
     );
---
+++ lib/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  @override
+  State<CustomBottomNavigationBar> createState() => _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  int selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: Colors.red,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.view_agenda),
+          label: 'Parties en cours',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.videogame_asset),
+          label: 'Jeux',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          label: 'Mon compte',
+        ),
+      ],
+      currentIndex: selectedIndex,
+      onTap: onItemTapped,
+    );
+  }
+}
